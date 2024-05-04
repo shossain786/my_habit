8e07a406f40e7b33577fdd9acd0d76180251227d
Saddam <sddmhossain@786gmail.com>
2024-05-04 10:14:33 +0530

    theme updated

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: kColorScheme,
         dropdownMenuTheme: DropdownMenuThemeData(
           textStyle: TextStyle(
             color: kColorScheme.onPrimaryContainer,
@@ -34,6 +33,56 @@ class MyApp extends StatelessWidget {
               BeveledRectangleBorder(),
             ),
           ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll(
+              // kColorScheme.onPrimaryContainer.withOpacity(0.3),
+              Colors.indigo..withOpacity(0.8),
+            ),
+            foregroundColor: MaterialStatePropertyAll(
+              kColorScheme.onSecondary,
+            ),
+            elevation: const MaterialStatePropertyAll(
+              20,
+            ),
+            shadowColor: const MaterialStatePropertyAll(
+              Colors.yellowAccent,
+            ),
+          ),
+        ),
+        colorScheme: kColorScheme.copyWith(
+          background: kColorScheme.onBackground,
+        ),
+        dataTableTheme: DataTableThemeData(
+          dataTextStyle: TextStyle(
+            color: kColorScheme.onSecondary,
+            fontSize: 16,
+          ),
+          decoration: BoxDecoration(
+            border: Border.symmetric(
+              horizontal: BorderSide(
+                width: 2,
+                color: kColorScheme.onSecondary,
+                style: BorderStyle.solid,
+              ),
+            ),
+          ),
+          headingTextStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.onSecondary,
+            wordSpacing: 4,
+            fontSize: 18,
+          ),
+          dataRowColor: MaterialStatePropertyAll(
+            kColorScheme.onPrimary.withOpacity(0.2),
+          ),
+          headingRowColor: const MaterialStatePropertyAll(
+            Colors.blue,
+          ),
+          dividerThickness: 2,
+          horizontalMargin: 10,
+          headingRowHeight: 40,
         ),
       ),
       home: const HomeScreen(),
